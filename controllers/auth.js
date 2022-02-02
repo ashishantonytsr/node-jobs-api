0f47f527c1d074519031c4003c810989358d0148
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-02 16:00:02 +0530

    added jwt token as mongoose instance method

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,8 @@ const User = require('../models/User')
 
 const register = async(req, res)=>{
 	const user = await User.create({...req.body})
-	res.status(StatusCodes.CREATED).json({user})
+	const token = user.createJWT()
+	res.status(StatusCodes.CREATED).json({ user: {name: user.name}, token })
 }
 
 const login = async(req, res)=>{
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = mongoose.Schema({
 	name:{
@@ -26,5 +27,13 @@ UserSchema.pre('save', async function(next){
 	this.password = await bcrypt.hash(this.password, salt)
 	next()
 })
+
+UserSchema.methods.createJWT = function(){
+	return token = jwt.sign(
+		{userId: this._id, name: this.name},
+		process.env.JWT_SECRET,
+		{expiresIn: '30d'}
+	)
+}
 
 module.exports = mongoose.model('User',UserSchema)
