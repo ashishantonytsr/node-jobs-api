e830412db5f80efffd73e60a0098208ab60926dc
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-05 23:12:37 +0530

    added third party security middlewares

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express');
 const app = express();
 
@@ -16,7 +23,18 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
+
 // extra packages
+app.use('trust proxy', 1)
+app.use(
+	rateLimiter({
+		windowMs: 15 * 60 * 1000, //15 minutes
+		max: 100 // limit each ip to 100 req per window	
+	})
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
 app.use('/api/v1/auth', authRouter)
