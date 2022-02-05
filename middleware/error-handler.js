a8171c1cbe5c753acc42728817a2908dd9db9ee7
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-05 22:25:26 +0530

    added error handler for mongoose errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,27 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json(
-			{ success: false, msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+	let errorObj = {
+		// set default
+		statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+		msg: err.message || 'Something went wrong. Try again later'
+	}
+
+	if (err.name === 'ValidationError'){
+		errorObj.msg = Object.values(err.errors)
+			.map(item=> item.message).join(', ')
+		errorObj.statusCode = StatusCodes.BAD_REQUEST
+	}
+	if (err.name === 'CastError'){
+		errorObj.msg = `no job found with id ${err.value}`
+		errorObj.statusCode = StatusCodes.BAD_REQUEST
+	}
+	if(err.code && err.code == 11000){
+		errorObj.msg = `duplicate value entered for ${Object.keys(err.keyValue)} field`
+		errorObj.statusCode = StatusCodes.BAD_REQUEST
+	}
+  return res.status(errorObj.statusCode).json({ msg: errorObj.msg })
 }
 
 module.exports = errorHandlerMiddleware
