93b8f603c431148998f926875045a41cd58a7750
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-05 17:05:44 +0530

    added controllers update & delete for jobs route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -36,11 +36,37 @@ const createJob = async(req, res)=>{
 }
 
 const updateJob = async(req, res)=>{
-	res.send('update jobs')
+	const {
+		user: {userId}, 
+		params: {id: jobId},
+		body: {company, position}
+	} = req
+	// checking if empty (fields not provided is fine)
+	if (company === '' || position === ''){
+		throw new BadRequestError('company or position fields cannot be empty')
+	}
+	const job = await Job.findOneAndUpdate(
+		{_id: jobId, createdBy: userId},
+		req.body,
+		{new: true, runValidators: true}
+	)
+	if (!job){
+		throw new NotFoundError(`no job with id ${jobId}`)
+	}
+	res.status(StatusCodes.OK).json({job})
+
 }
 
 const deleteJob = async(req, res)=>{
-	res.send('delete job')
+	const {
+		user : {userId},
+		params : {id: jobId}
+	} = req
+	const job = await Job.findOneAndDelete({_id: jobId, createdBy: userId})
+	if (!job){
+		throw new BadRequestError(`no job with id ${id}`)
+	}
+	res.status(StatusCodes.OK).json({job})
 }
 
 module.exports = {
