31dfe35878bdc5c702084c90c69bdef4857b9771
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-05 15:56:21 +0530

    added controllers for getAllJobs & createJob

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,10 @@
 const Job = require('../models/Job')
-const {StatusCodes, CREATED} = require('http-status-codes')
-const {BadRequestError, UnauthenticatedError} = require('../errors')
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError} = require('../errors')
 
 const getAllJobs = async(req, res)=>{
-	res.send('all jobs')
+	const jobs = await Job.find({createdBy: req.user.userId}).sort('createdAt')
+	res.status(StatusCodes.OK).json({jobs: jobs})
 }
 
 const getSingleJob = async(req, res)=>{
@@ -11,6 +12,15 @@ const getSingleJob = async(req, res)=>{
 }
 
 const createJob = async(req, res)=>{
+	// checking for duplicates
+	const jobDuplicate = await Job.find({createdBy: req.user.userId})
+		.where('company').equals(req.body.company)
+		.where('position').equals(req.body.position)
+	if (jobDuplicate != ''){
+		console.log(jobDuplicate);
+		throw new BadRequestError('duplicates found')
+	}
+
 	req.body.createdBy = req.user.userId
 	const job = await Job.create(req.body)
 	res.status(StatusCodes.CREATED).json({job})
