83a2163bd1a665d9dfa41302ea0d530a84c1bb4b
ashishantonytsr@gmail.com <ashishantonytsr@gmail.com>
2022-02-05 12:23:24 +0530

    added authentication for user

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const {UnauthenticatedError} = require('../errors')
+
+const authenticationMiddleware = async(req, res, next)=>{
+	// check header
+	const authHeader = req.headers.authorization
+	if (!authHeader || !authHeader.startsWith('Bearer ')){
+		throw new UnauthenticatedError('invalid authentication')
+	}
+	const token = authHeader.split(' ')[1]
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET)
+		// attach user to job routes
+		req.user = {userId: payload.userId, name:payload.name}
+		next()
+	} catch (error) {
+		throw new UnauthenticatedError('Authentication failed')
+	}
+}
+
+module.exports = authenticationMiddleware
